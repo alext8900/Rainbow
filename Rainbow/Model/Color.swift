c8c49a21458e2f58248def2f82d5c67e93e9ddec
Alex Thompson <guitarman226@gmail.com>
2019-09-17 21:45:07 -0500

    Added color model

--- Rainbow/Model/Color.swift
+++ Rainbow/Model/Color.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct Color {
+    let name: String
+    let color: UIColor
+}
--- Rainbow/ViewControllers/ColorsTableViewController.swift
+++ Rainbow/ViewControllers/ColorsTableViewController.swift
@@ -10,5 +10,17 @@ import UIKit
 
 class ColorsTableViewController: UITableViewController {
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+        
+        
+        cell.textLabel?.text = "Cell #\(indexPath.row + 1)"
+        return cell
+    }
+    
 }
 
