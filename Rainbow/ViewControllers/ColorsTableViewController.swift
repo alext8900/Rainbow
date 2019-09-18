91f1a1fc6a3c09686399a5a914fd7951c5bcbda9
Alex Thompson <guitarman226@gmail.com>
2019-09-17 21:52:08 -0500

    Added default colors to our app

--- Rainbow/ViewControllers/ColorsTableViewController.swift
+++ Rainbow/ViewControllers/ColorsTableViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class ColorsTableViewController: UITableViewController {
     
+    var colors: [Color] = [
+        Color(name: "Red", color: UIColor.red),
+        Color(name: "Orange", color: .orange),
+        Color(name: "Yellow", color: .yellow),
+        Color(name: "Green", color: .green),
+        Color(name: "Blue", color: .blue),
+        Color(name: "Cyan", color: .cyan),
+        Color(name: "Purple", color: .purple)
+    ]
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return colors.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
         
+        let color = colors[indexPath.row]
         
-        cell.textLabel?.text = "Cell #\(indexPath.row + 1)"
+        cell.textLabel?.text = color.name
         return cell
     }
     
